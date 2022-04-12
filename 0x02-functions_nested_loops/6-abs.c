3da80ea3ff1ac2d930cce6ac52b3e8505d6d581b
Abenezer-Tesfaye <abenzer9999@gmail.com>
2022-04-12 10:09:38 -0700

    betty documentation

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,18 +8,18 @@
  * Return: Always 0.
  */
 
-int _abs(int r)
+int _abs(int n)
 {
-if (r < 0)
+if (n < 0)
 {
-return (r * (-1));
+return (n * (-1));
 }
-else if (r == 0)
+else if (n == 0)
 {
 return (0);
 }
 else
 {
-return (r);
+return (n);
 }
 }
