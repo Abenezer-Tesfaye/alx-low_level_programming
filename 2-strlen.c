60a05b478360b919bf1009a63a5ef1e8580d06ca
Abenezer-Tesfaye <101438798+Abenezer-Tesfaye@users.noreply.github.com>
2022-04-18 16:55:37 +0300

    Update 2-strlen.c

--- 2-strlen.c
+++ 2-strlen.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
+ *_strlen -length of a string
+ *@s: A pointer to an int that will be changed/updated
+ *
+ *Return: void that means our answer is correct
  */
+
 int _strlen(char *s)
 {
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
+int u;
 
-return (inc);
+u = 0;
+while (s[u] != '\0')
+{
+u++;
+}
+return (u);
 }
